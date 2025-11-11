d3326ca15f67edb94ada80bc618bb295796f0a71
Kushuh <geoffroy.vincent@agorastoryverse.com>
2025-11-11 15:51:59 +0100

    chores: cleanup

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,12 +5,17 @@ set -e
 podman build --format docker \
   -f ./builds/database.Dockerfile \
   -t ghcr.io/a-novel/service-authentication/database:local .
+
+podman build --format docker \
+  -f ./builds/migrations.Dockerfile \
+  -t ghcr.io/a-novel/service-authentication/jobs/migrations:local .
+podman build --format docker \
+  -f ./builds/init.Dockerfile \
+  -t ghcr.io/a-novel/service-authentication/jobs/init:local .
+
 podman build --format docker \
   -f ./builds/rest.Dockerfile \
   -t ghcr.io/a-novel/service-authentication/rest:local .
 podman build --format docker \
   -f ./builds/standalone.Dockerfile \
   -t ghcr.io/a-novel/service-authentication/standalone:local .
-podman build --format docker \
-  -f ./builds/migrations.Dockerfile \
-  -t ghcr.io/a-novel/service-authentication/jobs/migrations:local .
