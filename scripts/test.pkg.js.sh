cb5a63384ef3d3648399b0a0d6433deb3c8a9364
Kushuh <geoffroy.vincent@agorastoryverse.com>
2026-03-06 15:23:25 +0100

    hotfix(scripts): wait for service to start in integration tests

--- scripts/test.pkg.js.sh
+++ scripts/test.pkg.js.sh
@@ -14,6 +14,16 @@ trap int_handler INT
 
 podman compose --podman-build-args='--format docker -q' -p "${APP_NAME}" -f "${PODMAN_FILE}" up -d --build
 
+MAX_RETRIES=30
+RETRIES=0
+until curl -s -o /dev/null -w "%{http_code}" "${REST_URL}/ping" | grep -q "200"; do
+    RETRIES=$((RETRIES+1))
+    [ $RETRIES -ge $MAX_RETRIES ] && exit 1
+    echo "Waiting for Authentication keys service on port ${REST_PORT}..."
+    sleep 2
+done
+echo "Authentication service is ready."
+
 pnpm test
 
 # Normal execution: containers are shut down.
