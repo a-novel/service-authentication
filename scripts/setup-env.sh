9380a6b2e647c53da47b0d6789b29b2333348870
Geoffroy Vincent <geoffroy.vincent@agorastoryverse.com>
2026-04-03 19:02:17 +0200

    chore(env): remove .envrc (#727)
    * chore(env): remove .envrc
    
    * fix: typo

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -5,6 +5,7 @@ export REST_PORT
 printf "Exposing Rest on port %s\n" "${REST_PORT}"
 POSTGRES_PORT="${POSTGRES_PORT:="$(node -e 'console.log(await (await import("get-port-please")).getRandomPort())')"}"
 export POSTGRES_PORT
+export SERVICE_JSON_KEYS_HOST="${SERVICE_JSON_KEYS_HOST:="localhost"}"
 SERVICE_JSON_KEYS_PORT="${SERVICE_JSON_KEYS_PORT:="$(node -e 'console.log(await (await import("get-port-please")).getRandomPort())')"}"
 export SERVICE_JSON_KEYS_PORT
 MAIL_UI_PORT="${MAIL_UI_PORT:="$(node -e 'console.log(await (await import("get-port-please")).getRandomPort())')"}"
@@ -12,7 +13,15 @@ export MAIL_UI_PORT
 PLATFORM_AUTH_PORT="${PLATFORM_AUTH_PORT:="$(node -e 'console.log(await (await import("get-port-please")).getRandomPort())')"}"
 export PLATFORM_AUTH_PORT
 
+export SUPER_ADMIN_EMAIL="${SUPER_ADMIN_EMAIL:="noreply@agorastoryverse.com"}"
+export SUPER_ADMIN_PASSWORD="${SUPER_ADMIN_PASSWORD:="admin"}"
+
 export REST_URL="${REST_URL:="http://localhost:${REST_PORT}"}"
-export MAIL_HOST=${MAIL_HOST:="http://localhost:${MAIL_UI_PORT}"}
-export PLATFORM_AUTH_URL=""${PLATFORM_AUTH_URL:="http://localhost:${PLATFORM_AUTH_PORT}"}""
+export MAIL_HOST="${MAIL_HOST:="http://localhost:${MAIL_UI_PORT}"}"
+export PLATFORM_AUTH_URL="${PLATFORM_AUTH_URL:="http://localhost:${PLATFORM_AUTH_PORT}"}"
+
+export POSTGRES_USER="${POSTGRES_USER:="postgres"}"
+export POSTGRES_PASSWORD="${POSTGRES_PASSWORD:="postgres"}"
+export POSTGRES_DB="${POSTGRES_DB:="authentication"}"
+export POSTGRES_HOST="${POSTGRES_HOST:="localhost"}"
 export POSTGRES_DSN="${POSTGRES_DSN:="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?sslmode=disable"}"
